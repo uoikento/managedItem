0547c7baa105887083a1e20a61949ca3687b8006
kento.uoi <fishperson19@gmail.com>
2020-12-25 11:02:24 +0900

    route1

---
+++ app/controllers/app_tops_controller.rb
@@ -0,0 +1,4 @@
+class AppTopsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/app_tops_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AppTopsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
